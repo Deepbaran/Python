0d8b53ce33e34bf6c80b489ebd557d8a2a15ca53
Deepbaran <deepbaran97@gmail.com>
2022-12-01 07:12:28 +0530

    regex notes

--- Tools/Regex.py
+++ Tools/Regex.py
@@ -1,6 +1,7 @@
 # Regex is majorly used for Pattern Matching
 # Regex Practice - https://regexr.com/
 # https://youtu.be/rhzKDrUiJVk
+# https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#:~:text=Regular%20expressions%20are%20patterns%20used,split()%20methods%20of%20String%20.
 # + ? * . \w \s \W \S \w{4} \w{4,} \w{4,5}
 # [fc] [a-z]
 # () | ^ $
