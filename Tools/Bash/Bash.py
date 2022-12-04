bf007b6559c0d34a66c8f844e6da0d51485714da
Deepbaran <deepbaran97@gmail.com>
2022-12-04 22:04:01 +0530

    added Bash content syllabus

--- Tools/Bash/Bash.py
+++ Tools/Bash/Bash.py
@@ -1,1 +1,25 @@
 # https://youtu.be/e7BufAVwDiM
+"""
+1. Hello Bash Scripting
+2. Redirect to file
+3. Comments
+4. Conditional Statements
+5. Loops
+6. Script input
+7. Scritp output
+8. Pipes
+9. Strings processing
+10. Numbers and Arithmetic
+11. Declare Command
+12. Arrays
+13. Functions
+14. Files and Directories
+15. Sending email via script
+16. Curl in Script
+17. Professional menus
+18. Wait for filesystem events with inotify
+19. Introduction to grep
+20. Introduction to awk
+21. Introduction to sed
+22. Debugging Bash Scripts
+"""
