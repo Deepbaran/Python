ad3cbb30de99152a2dd2755a7ec72ecc39e2116e
Deepbaran Kar <deepbarankar@Deepbarans-MacBook-Pro.local>
2023-03-08 09:33:13 +0530

    added some new notes

---
+++ Advanced/LibrariesAndNotes.py
@@ -0,0 +1,59 @@
+"""
+HEAP:
+
+min-heap - https://www.geeksforgeeks.org/heap-queue-or-heapq-in-python/
+max-heap - https://www.geeksforgeeks.org/max-heap-in-python/
+heapq - https://docs.python.org/3/library/heapq.html
+"""
+
+"""
+c = "a"
+print(ord(c)) # 97
+print(chr(97)) # 'a'
+"""
+
+"""
+enumerate - https://www.programiz.com/python-programming/methods/built-in/enumerate
+Counter - https://realpython.com/python-counter/#:~:text=Counter%20is%20a%20subclass%20of,argument%20to%20the%20class's%20constructor.
+"""
+
+"""
+sort - https://docs.python.org/3/howto/sorting.html
+"""
+
+"""
+Functional Programming in Python: When and How to Use It -https://realpython.com/python-functional-programming/
+Python Inner Functions - https://realpython.com/inner-functions-what-are-they-good-for/
+Recrusion in Python - https://realpython.com/python-recursion/
+lambda - https://realpython.com/python-lambda/
+map - https://realpython.com/python-map-function/
+filter - https://realpython.com/python-filter-function/
+reduce - https://realpython.com/python-reduce-function/
+
+- The use of recursion rather than loops or other structures as a primary flow control structure
+- A focus on lists or arrays processing
+- A focus on what is to be computed rather than on how to compute it
+- The use of pure function than avoid side effects
+- The use of higher-order-functions
+
+Recursion - Recursion is a technique in which functions call themselves, either directly or indirectly, in order to loop. It allows a program to loop over data structures that have unknown or unpredictable lengths.
+
+Pure Functions - Pure functions are functions that have no side effects at all. In other words, they’re functions that do not update or modify any global variable, object, or data structure in the program. These functions produce an output that depends only on the input, which is closer to the concept of a mathematical function.
+
+Higher Order Functions - Higher-order functions are functions that operate on other functions by taking functions as arguments, returning functions, or both, as with Python decorators.
+"""
+
+"""
+Primer on Python Decorators - https://realpython.com/primer-on-python-decorators/
+Recursion in Python - https://realpython.com/python-recursion/
+"""
+
+"""
+any - https://realpython.com/any-python/
+all - https://realpython.com/python-all/
+min max - https://realpython.com/python-min-and-max/
+"""
+
+"""
+functools - https://towardsdatascience.com/introducing-pythons-functools-module-2c4cba4774e
+"""
