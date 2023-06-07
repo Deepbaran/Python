0f4afee614541a294e36442982b0020e1ced192f
Deepbaran <deepbaran97@gmail.com>
2023-06-07 22:35:23 +0530

    added make_response in flask app

--- WebFrameworks/Flask/03-mySecondApp/application.py
+++ WebFrameworks/Flask/03-mySecondApp/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
@@ -30,40 +30,40 @@ tests_schema = TestSchema(many=True)
 @app.route('/test', methods=['POST'])
 def add_test():
     if 'id' in request.json:
-        return 'Only one attribute "name" is expected. Received an unknown attribte "id"'
+        return make_response('Only one attribute "name" is expected. Received an unknown attribte "id"', 400)
     else:
         new_test = Test(request.json['name'])
         db.session.add(new_test)
         db.session.commit()
-        return test_schema.jsonify(new_test)
+        return make_response(test_schema.jsonify(new_test), 200)
 
 @app.route('/test', methods=['GET'])
 def get_tests():
     res = Test.query.all()
     data = tests_schema.dump(res)
-    return jsonify(data)
+    return make_response(jsonify(data), 200)
 
 @app.route('/test/<id>', methods=['GET'])
 def get_test(id):
     res = Test.query.get(id)
-    return test_schema.jsonify(res)
+    return make_response(test_schema.jsonify(res), 200)
 
 @app.route('/test/<id>', methods=['PUT'])
 def update_test(id):
     if 'id' not in request.json:
-        return 'attribue "id" is required'
+        return make_response('attribue "id" is required', 400)
     else:
         res = Test.query.get(id)
         res.name = request.json["name"] if 'name' in request.json else res.name
         db.session.commit()
-        return test_schema.jsonify(res)
+        return make_response(test_schema.jsonify(res), 200)
 
 @app.route('/test/<id>', methods=['DELETE'])
 def delete_test(id):
     res = Test.query.get(id)
     db.session.delete(res)
     db.session.commit()
-    return test_schema.jsonify(res)
+    return make_response(test_schema.jsonify(res), 200)
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
